5fab5a5bf200cc010265e0b6c00df2948ec315cd
David Mohl <git@d.sh>
2022-06-21 21:39:51 +0900

    Implement generateUnstake/generateStake

--- src/hooks/useHandleStake.ts
+++ src/hooks/useHandleStake.ts
@@ -1,9 +1,61 @@
 import solido from '@chorusone/solido.js';
-import { Transaction, TransactionInstruction } from '@solana/web3.js';
+import { Connection, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js';
 import { useConnection, useMutation, usePublicKey, useSignAllTransactions } from 'saifu';
 
 import { findAssociatedTokenAddress } from '@/lib/ata';
 import { solToLamports } from '@/lib/number';
+
+export const generateStakeTransaction = async (
+  pk: PublicKey | undefined,
+  amount: number,
+  connection: Connection
+) => {
+  console.log('gen -- ', pk, amount);
+  if (!pk) {
+    return;
+  }
+
+  if (!amount) {
+    return;
+  }
+
+  const snapshot = await solido.getSnapshot(connection, solido.MAINNET_PROGRAM_ADDRESSES);
+  const stakeAmount = new solido.Lamports(amount);
+
+  const ata = await findAssociatedTokenAddress(pk, snapshot.programAddresses.stSolMintAddress);
+
+  // try to get ata
+  const insts: TransactionInstruction[] = [];
+
+  // add instruction to create ATA if doesn't exist yet
+  try {
+    connection.getAccountInfo(ata);
+  } catch (e) {
+    const ataInitInst = await solido.getATAInitializeInstruction(
+      snapshot.programAddresses.stSolMintAddress,
+      pk
+    );
+
+    insts.push(ataInitInst);
+  }
+
+  // actual deposit instruction
+  const depositInstruction = await solido.getDepositInstruction(
+    pk,
+    ata,
+    solido.MAINNET_PROGRAM_ADDRESSES,
+    stakeAmount
+  );
+
+  insts.push(depositInstruction);
+
+  const tx = new Transaction();
+  tx.add(...insts);
+  tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+  tx.feePayer = pk;
+
+  return tx;
+};
 
 const useHandleStake = () => {
   const connection = useConnection();
@@ -11,48 +63,11 @@ const useHandleStake = () => {
   const signAllTxs = useSignAllTransactions();
 
   return useMutation(async ({ enteredAmount }: { enteredAmount: number }) => {
-    if (!pk) {
+    const amount = solToLamports(enteredAmount);
+    const tx = await generateStakeTransaction(pk, amount, connection);
+    if (!tx) {
       return;
     }
-
-    if (!enteredAmount) {
-      return;
-    }
-
-    const snapshot = await solido.getSnapshot(connection, solido.MAINNET_PROGRAM_ADDRESSES);
-    const stakeAmount = new solido.Lamports(solToLamports(enteredAmount));
-
-    const ata = await findAssociatedTokenAddress(pk, snapshot.programAddresses.stSolMintAddress);
-
-    // try to get ata
-    const insts: TransactionInstruction[] = [];
-
-    // add instruction to create ATA if doesn't exist yet
-    try {
-      connection.getAccountInfo(ata);
-    } catch (e) {
-      const ataInitInst = await solido.getATAInitializeInstruction(
-        snapshot.programAddresses.stSolMintAddress,
-        pk
-      );
-
-      insts.push(ataInitInst);
-    }
-
-    // actual deposit instruction
-    const depositInstruction = await solido.getDepositInstruction(
-      pk,
-      ata,
-      solido.MAINNET_PROGRAM_ADDRESSES,
-      stakeAmount
-    );
-
-    insts.push(depositInstruction);
-
-    const tx = new Transaction();
-    tx.add(...insts);
-    tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-    tx.feePayer = pk;
 
     signAllTxs([tx]);
   });
